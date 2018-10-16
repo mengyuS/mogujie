f96d9dbec472eabc2ebfa505854fd0b952589844
mengyuS <992540162@qq.com>
2018-10-16 18:39:31 +0800

    项目首页提交2

--- dist/texiao.js
+++ dist/texiao.js
@@ -35,9 +35,7 @@ second.innerHTML=(ssecond);
 
  //无缝轮播  秒杀后面
   
- $(function(){
-		
-	
+ $(function(){			
     function Banner(){}
       $.extend(Banner.prototype,{
           // 写法是兼容的;
@@ -53,19 +51,16 @@ second.innerHTML=(ssecond);
              this.nowIndex = 0;
              // 有多少元素;
              this.item_num = this.item_list.length;
-             this.ul = $(".show ul");
+            this.ul = $(".show ul");
+            //  this.ul=$(options.ul)
              // 获取列表中第一个元素的宽度值;
              this.item_width = this.item_list.width();
-              
-             
+            //this.item_width = $(".show li").css("width");          
              this.bindEvent();
           },
-          bindEvent : function(){
-             
+          bindEvent : function(){           
               this.left_btn.click($.proxy(this.prev , this));
               this.right_btn.click($.proxy(this.next , this));
-            
-
           },
           next:function(){
               if( this.nowIndex == this.item_num - 1){
@@ -123,16 +118,114 @@ second.innerHTML=(ssecond);
       })
 
       var banner = new Banner();
-
       banner.init({
           item_list : ".show li",
           left_btn : "#left",
           right_btn : "#right",
+         
         
       })
       banner.autoPlay();
 
-    
-
+    //   var banner2 = new Banner();
+    //   banner2.init({
+    //       item_list : ".show2 li",
+    //       left_btn : "#left",
+    //       right_btn : "#right",
+    //       ul:".show2 ul"
+        
+    //   })
+    //   banner2.autoPlay();
   })
 
+
+  //女装部分轮播图；
+  $(function(){			
+    function Bannergirl(){}
+      $.extend(Bannergirl.prototype,{
+          // 写法是兼容的;
+          init : function(options){
+             // 所有的图片;
+             this.item_list = $(options.item_list);
+             // 左按钮 ;
+             this.left_btn = $(options.left_btn);
+             // 右按钮 ;
+             this.right_btn = $(options.right_btn);
+             // 按钮列表;
+             //this.btn_list = $(options.btn_list);
+             this.nowIndex = 0;
+             // 有多少元素;
+             this.item_num = this.item_list.length;
+             this.ul = $(".show2 ul");
+             // 获取列表中第一个元素的宽度值;
+             this.item_width = this.item_list.width();          
+             this.bindEvent();
+          },
+          bindEvent : function(){           
+              this.left_btn.click($.proxy(this.prev , this));
+              this.right_btn.click($.proxy(this.next , this));
+          },
+          next:function(){
+              if( this.nowIndex == this.item_num - 1){
+                  this.nowIndex = 1;
+                  this.ul.css({
+                      left : 0
+                  })
+               
+              }else{
+                  this.nowIndex ++;
+              }
+              this.animate();
+          },
+          prev:function(){
+              // console.log(this);
+              if( this.nowIndex == 0){
+                  this.nowIndex = this.item_num - 2;
+                  this.ul.css({
+                      left : -(this.item_num - 1) * this.item_width
+                  })
+              }else{
+                  this.nowIndex --;
+              }
+
+              this.animate();
+          },
+       
+          animate:function(){
+              // console.log(this.nowIndex);
+              this.ul.stop().animate({
+                  left : -950 * this.nowIndex
+              })
+       
+          },
+         
+          stopPlay:function(){
+              clearInterval(this.autoPlay_timer);
+        
+          },
+          autoPlay : function(){
+            this.time = setInterval(function(){
+                // this.next()
+                if(this.nowIndex == this.item_num-1){
+                    this.nowIndex = 1;
+                    this.ul.css({
+                        left: 0
+                    })
+                }else{
+                    this.nowIndex ++;
+                }
+                this.animate();
+            }.bind(this),5000)
+    
+        }
+      })
+
+      var banner = new Bannergirl();
+      banner.init({
+          item_list : ".show2 li",
+          left_btn : "#left2",
+          right_btn : "#right2",
+        
+      })
+      banner.autoPlay();
+  })
